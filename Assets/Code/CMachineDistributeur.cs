800827d9544f864c4d1c130a83d21f4d918e913f
Khoyo <khoyobegenn@gmail.com>
2013-10-17 10:20:53 +0200

    Reparation connerie tim ^^

--- Assets/Code/CMachineDistributeur.cs
+++ Assets/Code/CMachineDistributeur.cs
@@ -5,12 +5,15 @@ public class CMachineDistributeur : MonoBehaviour, IMachineAction {
 	
 	public GameObject prefabDistribue;
 	
+	bool givingOnNextTurn;
+	GameObject m_cannette;
+	
+	
 	public void Activate(CPlayer player){
-		
-		GameObject cannette = GameObject.Instantiate(prefabDistribue) as GameObject;
-		CScriptTakeElement elem = cannette.GetComponent<CScriptTakeElement>();
-		elem.GetTakeElement().Init();
-		player.PickUpObject(elem.GetTakeElement());
-		
+		m_cannette = GameObject.Instantiate(prefabDistribue) as GameObject;		
 	}
+	
+	public void Process(){
+	}
+	
 }
