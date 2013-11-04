900277feb632ed89363c2caf621f42643b1400a2
LeuhFahn <leuh.fahn@gmail.com>
2013-11-04 16:40:50 +0100

    creep fonctionnel

--- Assets/Code/CCreep.cs
+++ Assets/Code/CCreep.cs
@@ -3,6 +3,14 @@ using System.Collections;
 
 public class CCreep : CElement 
 {
+	enum EState
+	{
+		e_Nothing,
+		e_FollowPlayer,
+		e_TakePlayer,
+		e_OnPlayer
+	}
+	
 	CScriptCreep m_ScriptCreep;
 	CScriptCreepZone m_ScriptCreepZone;
 	CSpriteSheet m_SpriteSheet;
@@ -12,9 +20,11 @@ public class CCreep : CElement
 	bool m_bCanParasitizedPlayer;
 	bool m_bFollowPlayer;
 	float m_fTimerParasite;
+	float m_fTimerTakePlayer;
+	const float m_fTimerTakePlayerMax = 1.0f;
 	float m_fVelocity;
 	Vector3 m_posToGo;
-	
+	EState m_eState;
 
 	//-------------------------------------------------------------------------------
 	///
@@ -26,6 +36,8 @@ public class CCreep : CElement
 		m_bCanParasitizedPlayer = true;
 		m_fTimerParasite = 0.0f;
 		m_fVelocity = 0.0f;
+		m_fTimerTakePlayer = 0.0f;
+		m_eState = EState.e_Nothing;
 	}
 	
 	//-------------------------------------------------------------------------------
@@ -64,6 +76,7 @@ public class CCreep : CElement
 		m_bIsOnPlayer = false;
 		m_bCanParasitizedPlayer = true;
 		m_fTimerParasite = 0.0f;
+		m_eState = EState.e_Nothing;
 	}
 
 	//-------------------------------------------------------------------------------
@@ -73,32 +86,54 @@ public class CCreep : CElement
 	{
 		base.Process(fDeltatime);
 		m_SpriteSheet.Process();
-		if(m_bIsOnPlayer)
+		
+		Vector3 velocity = new Vector3(0,0,0);
+		switch(m_eState)
 		{
-			m_fTimerParasite += fDeltatime;
-			SetPosition2D(m_PlayerParasitized.GetPosition2D());
-			if(m_fTimerParasite > m_Game.m_fCreepTimerParasiteMin)
-				m_bCanParasitizedPlayer = true;
-			if(m_fTimerParasite > m_Game.m_fCreepTimerParasiteMax)
-				LeavePlayer();
-		}
-		else  
-		{
-			Vector3 velocity = new Vector3(0,0,0);
-			if(m_bFollowPlayer)
-			{
-				velocity = (m_posToGo - m_GameObject.transform.position).normalized;
-			}
-			else
+			case EState.e_Nothing:
 			{
 				Vector2 rand = Random.insideUnitCircle;
 				velocity.x = rand.x;
 				velocity.y = rand.y;
+				break;	
 			}
 			
-			m_GameObject.transform.position += m_fVelocity * velocity * fDeltatime;
+			case EState.e_FollowPlayer:
+			{
+				velocity = (m_posToGo - m_GameObject.transform.position).normalized;
+				break;	
+			}
 			
+			case EState.e_TakePlayer:
+			{
+				m_fTimerTakePlayer += fDeltatime;
+				
+				Vector2 posCreep = new Vector2(m_GameObject.transform.position.x, m_GameObject.transform.position.y);
+			
+				float fX = CApoilMath.InterpolationLinear(m_fTimerTakePlayer, 0.0f, m_fTimerTakePlayerMax, posCreep.x, m_PlayerParasitized.GetPosition2D().x);
+				float fY = CApoilMath.InterpolationLinear(m_fTimerTakePlayer, 0.0f, m_fTimerTakePlayerMax, posCreep.y, m_PlayerParasitized.GetPosition2D().y);
+				
+				SetPosition2D(new Vector2(fX, fY));
+			
+				if(m_fTimerTakePlayer > m_fTimerTakePlayerMax)
+					m_eState = EState.e_OnPlayer;
+				break;	
+			}
+			
+			case EState.e_OnPlayer:
+			{
+				m_fTimerParasite += fDeltatime;
+				SetPosition2D(m_PlayerParasitized.GetPosition2D());
+				if(m_fTimerParasite > m_Game.m_fCreepTimerParasiteMin)
+					m_bCanParasitizedPlayer = true;
+				if(m_fTimerParasite > m_Game.m_fCreepTimerParasiteMax)
+					LeavePlayer();
+				break;	
+			}
 		}
+		
+		m_GameObject.transform.position += m_fVelocity * velocity * fDeltatime;
+
 	}
 	
 	//-------------------------------------------------------------------------------
@@ -112,30 +147,37 @@ public class CCreep : CElement
 	public void SetPositionToGo(Vector2 pos)
 	{
 		m_posToGo = new Vector3(pos.x, pos.y, 0.0f);
-		m_bFollowPlayer = true;
+		m_eState = EState.e_FollowPlayer;
 	}
 	
 	public void StopFollowPlayer()
 	{
 		m_posToGo = Vector3.zero;
-		m_bFollowPlayer = false;		
+		m_eState = EState.e_Nothing;		
 	}
 	
 	public void SetPlayerParasitized(CPlayer player)
 	{
+		LeavePlayer();
 		m_PlayerParasitized = player;
 		m_PlayerParasitized.SetCreepOnPlayer(this);
 		m_bIsOnPlayer = true;
 		m_fTimerParasite = 0.0f;
 		m_bCanParasitizedPlayer = false;
+		m_eState = EState.e_TakePlayer;
+		m_fTimerTakePlayer = 0.0f;
 	}	
 	
 	public void LeavePlayer()
 	{
-		m_PlayerParasitized.CreepLeavePlayer();
-		m_PlayerParasitized = null;		
-		m_bIsOnPlayer = false;
-		m_bCanParasitizedPlayer = true;
+		if(m_PlayerParasitized != null)
+		{
+			m_PlayerParasitized.CreepLeavePlayer();
+			m_PlayerParasitized = null;		
+			m_bIsOnPlayer = false;
+			m_bCanParasitizedPlayer = true;
+			m_eState = EState.e_Nothing;
+		}
 	}
 		
 	public bool IsOnPlayer()
