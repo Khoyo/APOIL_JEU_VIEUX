59755b84b40f7554238e7d17c4386cfe3540e98c
LeuhFahn <leuh.fahn@gmail.com>
2013-11-08 10:26:50 +0100

    scale texture mur

--- Assets/Code/CScriptMur.cs
+++ Assets/Code/CScriptMur.cs
@@ -1,15 +1,19 @@
 using UnityEngine;
 using System.Collections;
 
-public class CScriptMur : MonoBehaviour {
+public class CScriptMur : MonoBehaviour 
+{
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		float fY = gameObject.transform.localScale.y / gameObject.transform.localScale.x;
+		gameObject.renderer.material.SetTextureScale("_MainTex", new Vector2(1,fY));
 	}
 }
