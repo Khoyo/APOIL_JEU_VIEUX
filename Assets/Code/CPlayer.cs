095679d975413086e0e1a6378f8dd0f8be1211ac
LeuhFahn <leuh.fahn@gmail.com>
2013-11-14 15:48:47 +0100

    player p^hysique move

--- Assets/Code/CPlayer.cs
+++ Assets/Code/CPlayer.cs
@@ -473,8 +473,9 @@ public class CPlayer : CCharacter
 				MagnetDeviation.x = Deviation.x;
 				MagnetDeviation.y = Deviation.y;
 			}
-				
-			m_GameObject.transform.position += m_fSpeed * velocity * fDeltatime + m_fForceMagnet * MagnetDeviation;
+			//velocity+=  m_fForceMagnet * MagnetDeviation;
+			m_GameObject.transform.position += /*m_fSpeed * velocity * fDeltatime + */m_fForceMagnet * MagnetDeviation;
+			m_GameObject.rigidbody.velocity = m_fSpeed * velocity;
 		}
 		else
 		{
