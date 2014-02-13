4178ba821dc83e56be80069b93e34612fff11e57
leuhfahn <leuh.fahn@gmail.com>
2014-02-13 15:25:35 -0800

    create gameCreator

---
+++ Assets/Code/CGameCreator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CGameCreator : MonoBehaviour {
+
+	static int m_instanceCount = 0;
+	public GameObject m_prefabGame;
+
+	void Awake() {
+		if(m_instanceCount++ == 0)
+		{
+			CGame game = ((GameObject) GameObject.Instantiate(m_prefabGame)).GetComponent<CGame>();
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void OnDestroy() {
+		m_instanceCount--;
+	}
+}
